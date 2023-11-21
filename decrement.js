3c254cf60a8ac244890ba5ecc931ed09d6465c09
sagarpp28 <sagarpp28@gmail.com>
2023-11-21 15:47:49 +0530

    first commit

---
+++ decrement.js
@@ -0,0 +1,4 @@
+let readlineSync = require('readline-sync');
+let x = parseInt(readlineSync.question('Enter a number '));
+x--;
+console.log('Result is',z=x);
---
+++ division.js
@@ -0,0 +1,4 @@
+let readlineSync = require('readline-sync');
+let fno = parseInt(readlineSync.question('First number '));
+let sno = parseInt(readlineSync.question('Second number '));
+console.log('Result is', fno/sno);
